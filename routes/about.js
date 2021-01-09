8d66ff2dafc80436762f28ad23715868e04f6312
Apurva Kawale <apurvakawale1@gmail.com>
2021-01-09 11:38:03 +0530

    commit with changes

--- routes/about.js
+++ routes/about.js
@@ -2,8 +2,10 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function(req, res) {
-    res.render('search', {
-        andaman: 'Andaman and Nicobar'
+    res.render('about', {
+        jagruti: 'Jagruti Manwar',
+        apurva: 'Apurva Kawale',
+        rucha: 'Rucha Damre'
     });
 });
 
